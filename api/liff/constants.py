54bacfdc9a6a265dea312b9c1eaec66def832b9f
ALFINO NH <39095075+ALFINONH@users.noreply.github.com>
2019-05-14 23:18:48 +0700

    Add files via upload

---
+++ api/liff/constants.py
@@ -0,0 +1,5 @@
+from thrift.Thrift import TType, TMessageType, TFrozenDict, TException, TApplicationException
+from thrift.protocol.TProtocol import TProtocolException
+
+import sys
+from .ttypes import *
